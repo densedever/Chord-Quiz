5cb1858863754e1c43188204577bcbf9c42a76e6
densedever <dylanmrczk243@gmail.com>
2024-09-04 22:18:55 -0500

    Added buttons
    Made buttons to play different chords under Practice section

--- chordquiz.py
+++ chordquiz.py
@@ -14,12 +14,13 @@ Names in uppercase denote constants
 Names are in snake_case otherwise.
 """
 
-# TODO: make list/dict of supported chord colors and display them when played
+# TODO: make quiz page
 
 
 import pygame
 import os
 import re
+import random
 
 ### setup
 pygame.init()
@@ -54,14 +55,47 @@ def human_sort(l):
 SOUND_FILE_NAMES = human_sort([file for file in os.listdir(".\\sounds\\")])
 NOTES = [pygame.mixer.Sound(".\\sounds\\" + file) for file in SOUND_FILE_NAMES]
 
+class Button():
+    # all these values need tweaking to look nicer
+    # going for functionality over looks for now
+    def __init__(self, txt, pos):
+        self.text     = txt
+        self.pos      = pos
+        button_width  = 230
+        button_height = 40
+        self.button   = pygame.rect.Rect((self.pos[0], self.pos[1]), (button_width, button_height))
+    
+    def draw(self):
+        pygame.draw.rect(screen, 'light grey', self.button, 0, 5)
+        pygame.draw.rect(screen, 'dark grey', self.button, 5, 5)
+        text = font.render(self.text, True, 'black')
+        screen.blit(text, (self.pos[0] + 15, self.pos[1] + 7))
+        
+    def check_clicked(self):
+        return (self.button.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0])
+
 
 ### tools for building chords
+
+CHORD_COLORS = {
+    "maj":  [4, 3],
+    "min":  [3, 4],
+    "dim":  [3, 3],
+    "aug":  [4, 4],
+    "sus2": [2, 5],
+    "sus4": [5, 2],
+    "maj7": [4, 3, 4],
+    "min7": [3, 4, 3],
+    "dim7": [3, 3, 3],
+    "dom7": [4, 3, 3],
+    "maj9": [4, 3, 4, 3],
+    "min9": [3, 4, 3, 4],
+}
 
 class Chord():
     def __init__(self, root, color):
         self.root  = root # root note as sound file
         self.color = color # 'color' means the type of chord
-        #self.name  = "" # display name of chord, such as 'Eb-9'
         
         # the following calculates the notes played on a piano
         # using a cumulative sum stored in self.notes
@@ -79,44 +113,40 @@ class Chord():
     def play(self):
         for n in self.notes:
             NOTES[n].play(1, NOTE_DURATION)
-    
-    # here because sounds weren't playing right, and I might need later.
-    """
+
     def debug(self):
         for n in self.notes:
             print(SOUND_FILE_NAMES[n])
-    """
 
-class Button():
-    # all these values need tweaking to look nicer
-    # going for functionality over looks for now
-    def __init__(self, txt, pos):
-        self.text     = txt
-        self.pos      = pos
-        button_width  = 200
-        button_height = 40
-        self.button   = pygame.rect.Rect((self.pos[0], self.pos[1]), (button_width, button_height))
-    
-    def draw(self):
-        pygame.draw.rect(screen, 'light grey', self.button, 0, 5)
-        pygame.draw.rect(screen, 'dark grey', self.button, 5, 5)
-        text = font.render(self.text, True, 'black')
-        screen.blit(text, (self.pos[0] + 15, self.pos[1] + 7))
-        
-    def check_clicked(self):
-        return (self.button.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0])
+
+
 
 
 # buttons for navigation and playing the chords
 practice = Button("Practice", (SCREEN_WIDTH/2, SCREEN_HEIGHT/2 - SCREEN_HEIGHT/6))
-quiz     = Button("Quiz", (SCREEN_WIDTH/2, SCREEN_HEIGHT/2 + SCREEN_HEIGHT/6))
-play     = Button("Play", (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
 
-# a test value and later more chord colors to pass into the Chord() class
-MAJOR_TRIAD = [4, 3]
 
-# C4 major triad starting at root 39, going up by 4 and then 3 semitones 
-example_chord = Chord(39, MAJOR_TRIAD) 
+
+CHORD_BUTTONS = {
+    "maj":  Button("Major",        ((SCREEN_WIDTH/3)*0, (SCREEN_HEIGHT/4)*0)),
+    "min":  Button("Minor",        ((SCREEN_WIDTH/3)*1, (SCREEN_HEIGHT/4)*0)),
+    "dim":  Button("Diminished",   ((SCREEN_WIDTH/3)*2, (SCREEN_HEIGHT/4)*0)),
+    "aug":  Button("Augmented",    ((SCREEN_WIDTH/3)*0, (SCREEN_HEIGHT/4)*1)),
+    "sus2": Button("Suspended 2",  ((SCREEN_WIDTH/3)*1, (SCREEN_HEIGHT/4)*1)),
+    "sus4": Button("Suspended 4",  ((SCREEN_WIDTH/3)*2, (SCREEN_HEIGHT/4)*1)),
+    "maj7": Button("Major 7",      ((SCREEN_WIDTH/3)*0, (SCREEN_HEIGHT/4)*2)),
+    "min7": Button("Minor 7",      ((SCREEN_WIDTH/3)*1, (SCREEN_HEIGHT/4)*2)),
+    "dim7": Button("Diminished 7", ((SCREEN_WIDTH/3)*2, (SCREEN_HEIGHT/4)*2)),
+    "dom7": Button("Dominant 7",   ((SCREEN_WIDTH/3)*0, (SCREEN_HEIGHT/4)*3)),
+    "maj9": Button("Major 9",      ((SCREEN_WIDTH/3)*1, (SCREEN_HEIGHT/4)*3)),
+    "min9": Button("Minor 9",      ((SCREEN_WIDTH/3)*2, (SCREEN_HEIGHT/4)*3))
+}
+
+quiz = Button("Quiz", (SCREEN_WIDTH/2, SCREEN_HEIGHT/2 + SCREEN_HEIGHT/6))
+play = Button("Play", (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+
+# setting a comfortable hearing range for the chords
+random_note_range = random.randint(20, 60)
 
 ### main
 running = True
@@ -132,9 +162,15 @@ while running:
             navigation = QUIZ
     
     if navigation == PRACTICE:
-        play.draw()
-        if play.check_clicked():
-            example_chord.play()
+        for v in CHORD_BUTTONS.values():
+            v.draw()
+        for k, v in CHORD_BUTTONS.items():
+            if v.check_clicked():
+                for k2, v2 in CHORD_COLORS.items():
+                    if k == k2:
+                        ch = Chord(random_note_range, v2)
+                        ch.play()
+                        ch.debug()
 
     # all events here
     for event in pygame.event.get():
